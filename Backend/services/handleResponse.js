7233ef23e8f1be9d0526c2256c90b1201860b2f6
akshat497 <122341089+akshat497@users.noreply.github.com>
2023-12-01 22:18:38 +0530

    delete

--- Backend/services/handleResponse.js
+++
@@ -1,9 +0,0 @@
-// response.js
-
-const handleResponse = (res, success, data = null, error = null) => {
-    const status = success ? 200 : 400;
-    return res.status(status).json({ success, data, error });
-  };
-  
-  module.exports = handleResponse;
-  
